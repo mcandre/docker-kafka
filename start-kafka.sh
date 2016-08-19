4c5e5e19ab32ebb261e52676a892f446d89ac753
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-08-19 16:19:49 -0500

    editorconfig

--- start-kafka.sh
+++ start-kafka.sh
@@ -5,8 +5,8 @@ CONFIG="${KAFKA_HOME}/config/server.properties"
 ZOOKEEPER="${DOCKERKAFKA_ZOOKEEPER_1_PORT_2181_TCP_ADDR}:${DOCKERKAFKA_ZOOKEEPER_1_PORT_2181_TCP_PORT}"
 
 sed -i \
-  -e "s/zookeeper.connect=.*/zookeeper.connect=${ZOOKEEPER}/" \
-  -e "s/\(#\s*\)\{0,1\}advertised.host.name=.*/advertised.host.name=${KAFKA_ADVERTISED_HOST_NAME}/" \
-  $CONFIG
+    -e "s/zookeeper.connect=.*/zookeeper.connect=${ZOOKEEPER}/" \
+    -e "s/\(#\s*\)\{0,1\}advertised.host.name=.*/advertised.host.name=${KAFKA_ADVERTISED_HOST_NAME}/" \
+    $CONFIG
 
 kafka-server-start.sh $CONFIG
