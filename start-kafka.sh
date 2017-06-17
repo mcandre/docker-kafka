b74654a8e8925c9dc05205c096876417651e9eb2
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-06-16 23:27:46 -0500

    isolate bashisms

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -euo pipefail
 
 CONFIG="${KAFKA_HOME}/config/server.properties"
@@ -8,6 +8,6 @@ ZOOKEEPER="${DOCKERKAFKA_ZOOKEEPER_1_PORT_2181_TCP_ADDR}:${DOCKERKAFKA_ZOOKEEPER
 sed -i \
     -e "s/zookeeper.connect=.*/zookeeper.connect=${ZOOKEEPER}/" \
     -e "s/\(#\s*\)\{0,1\}advertised.host.name=.*/advertised.host.name=${KAFKA_ADVERTISED_HOST_NAME}/" \
-    $CONFIG
+    "$CONFIG"
 
-kafka-server-start.sh $CONFIG
+kafka-server-start.sh "$CONFIG"
